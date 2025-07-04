c0d28eb51863f76fed8996e527b60245c4f4b6d8
Sprite86835 <Marcel198604@web.de>
2025-07-04 23:52:04 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -66,4 +66,6 @@ def check_lotto():
     return Response(out.getvalue(), content_type=f'multipart/mixed; boundary="{multipart.get_boundary()}"')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
